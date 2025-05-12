5c5f6a015d23ca2e260d8759d3286980d6889aeb
Firoz <firozshaikh829@gmail.com>
2025-05-12 22:11:56 +0530

    todo list project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todolist, setTodolist] = useState([]);
+  // console.log(input);
+  const addTodoList = () => {
+    if (input.trim() === "") return;
+    const item = {
+      id: todolist.length + 1,
+      text: input.trim(),
+      completed: false,
+    };
+    setTodolist((prev) => [...prev, item]);
+    setInput("");
+  };
+
+  const toggleCompleted = (id) => {
+    setTodolist(
+      todolist.map((t) => {
+        if (t.id === id) {
+          return {
+            ...t,
+            completed: !t.completed,
+          };
+        } else {
+          return t;
+        }
+      })
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodolist(todolist.filter((e) => e.id !== id));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <input
+        className="input"
+        type="text"
+        placeholder="Enter Todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button className="addbtn" onClick={() => addTodoList()}>
+        Add
+      </button>
+      <h2>Task List</h2>
+      <ul>
+        {todolist.map((t) => (
+          <li key={t.id}>
+            <input
+              type="checkbox"
+              checked={t.completed}
+              onChange={() => toggleCompleted(t.id)}
+            />
+            <span className={t.completed ? "strickThrough" : ""}>{t.text}</span>
+            <button onClick={() => deleteTodo(t.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
